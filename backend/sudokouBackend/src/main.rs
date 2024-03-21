fb20961639e5138a65f22b77eeee0ffaebce4b9e
T.Arthur <tcheutchouaarthur38@gmail.com>
2024-03-21 11:04:52 +0100

    sudoku-grid-board correction

--- backend/sudokouBackend/src/main.rs
+++ backend/sudokouBackend/src/main.rs
@@ -1,42 +1,6 @@
-#[derive(Clone, Debug)]
-struct SudokuGrid {
-    data: [[u8; 9]; 9], // 9x9 grid to hold Sudoku numbers
-}
+mod board;
 
-impl SudokuGrid {
-    fn new() -> Self {
-        SudokuGrid { data: [[0; 9]; 9] }
-    }
-
-    fn set_number(&mut self, row: usize, col: usize, number: u8) -> Result<(), &'static str> {
-        if !Self::is_valid_position(row, col) {
-            return Err("Invalid position");
-        }
-        self.data[row][col] = number;
-        Ok(())
-    }
-
-    fn get_number(&self, row: usize, col: usize) -> Option<u8> {
-        if Self::is_valid_position(row, col) {
-            Some(self.data[row][col])
-        } else {
-            None
-        }
-    }
-
-    fn is_valid_position(row: usize, col: usize) -> bool {
-        row < 9 && col < 9
-    }
-
-    fn print_grid(&self) {
-        for row in &self.data {
-            for &cell in row {
-                print!("{} ", cell);
-            }
-            println!();
-        }
-    }
-}
+use board::{SudokuGrid, SudokuError};
 
 fn main() {
     let mut sudoku = SudokuGrid::new();
