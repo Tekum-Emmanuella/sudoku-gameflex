5a61fb4580144d38a1827e842822894b022793b2
ndefokou <150008782+ndefokou@users.noreply.github.com>
2024-03-26 08:40:03 +0100

    Update main.rs

--- backend/sudokouBackend/src/main.rs
+++ backend/sudokouBackend/src/main.rs
@@ -1,49 +1,9 @@
 mod grid;
 
 use grid::{SudokuGrid, SudokuError};
-use serde::{Serialize, Deserialize}; // Added serde traits
-
-#[derive(Clone, Debug, Serialize, Deserialize)] // Added serde traits
-struct SudokuGrid {
-    data: [[u8; 9]; 9], // 9x9 grid to hold Sudoku numbers
-}
-
-impl SudokuGrid {
-    fn new() -> Self {
-        SudokuGrid { data: [[0; 9]; 9] }
-    }
-
-    fn set_number(&mut self, row: usize, col: usize, number: u8) -> Result<(), SudokuError> { // Changed return type to Result
-        if !Self::is_valid_position(row, col) {
-            return Err(SudokuError::InvalidPosition); // Changed error type
-        }
-        self.data[row][col] = number;
-        Ok(())
-    }
-
-    fn get_number(&self, row: usize, col: usize) -> Option<u8> {
-        if Self::is_valid_position(row, col) {
-            Some(self.data[row][col])
-        } else {
-            None
-        }
-    }
-
-    fn is_valid_position(row: usize, col: usize) -> bool {
-        row < 9 && col < 9
-    }
-
-    fn print_grid(&self) {
-        for row in &self.data {
-            for &cell in row {
-                print!("{} ", cell);
-            }
-            println!();
-        }
-    }
-}
 
 fn main() {
+    // Example usage:
     let mut sudoku = SudokuGrid::new();
     sudoku.set_number(0, 0, 5).unwrap();
     sudoku.set_number(0, 2, 8).unwrap();
